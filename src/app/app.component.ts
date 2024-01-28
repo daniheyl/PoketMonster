f7353d1c2ef8ea4891c50de6577cc8363070b0f9
daniheyl <35920095+daniheyl@users.noreply.github.com>
2024-01-28 23:52:23 +0100

    made a nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { PokedexComponent } from './pokedex/pokedex.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 import { AuthService } from './auth-service.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PokemonComponent, PokedexComponent],
+  imports: [CommonModule, RouterOutlet, PokemonComponent, PokedexComponent, LoginComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
